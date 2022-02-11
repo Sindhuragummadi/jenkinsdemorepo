fdbd82be2d9fccc8ecf8bd84d9375a741a2d3aab
Sindhuragummadi <98617123+Sindhuragummadi@users.noreply.github.com>
2022-02-11 10:57:20 -0600

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,8 +1,5 @@
 public class Sample {
-
-public static void main(String[] args) {
-
-System.out.println("A sample java program ");
- 
-   }
+  public static void main(String[] args) {
+    System.out.println("This is a Sample message");
+  }
 }
