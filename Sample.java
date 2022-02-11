810bebb2d937d26bba9667e24f57b202864bc0e4
Sindhuragummadi <98617123+Sindhuragummadi@users.noreply.github.com>
2022-02-11 06:24:28 -0600

    updated sample java program

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ public class Sample{
 
 public static void main(String[] args){
 
-System.out.println("sample,Program");
+System.out.println("sample java program ");
   System.out.println("second line added for test");
 }
 
